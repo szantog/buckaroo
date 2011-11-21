b07fe88c76c896cefd11cd8b909f702f60e93870
Gabor Szanto <hello@szantogabor.com>
2011-11-21 06:22:33 +0100

    Refactor get buckaroo variables and add test mode

--- theme/buckaroo-form.tpl.php
+++ theme/buckaroo-form.tpl.php
@@ -6,4 +6,8 @@
     <h3 class="title"><?php print $amount_title;?></h3>
   <?php endif; ?>
   <?php print render($amount_form) ?>
+
+  <?php if ($method_title) : ?>
+    <h3 class="title"><?php print $method_title;?></h3>
+  <?php endif; ?>
 </div>
