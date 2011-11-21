6fee99f588e37708db07c9d2644b8058f9666f0f
Gabor Szanto <hello@szantogabor.com>
2011-11-21 07:48:30 +0100

    Add uniqe forms to template

--- theme/buckaroo-form.tpl.php
+++ theme/buckaroo-form.tpl.php
@@ -10,4 +10,7 @@
   <?php if ($method_title) : ?>
     <h3 class="title"><?php print $method_title;?></h3>
   <?php endif; ?>
+  <?php foreach ($method_forms as $method) :?>
+    <?php print render($method); ?>
+  <?php endforeach;?>
 </div>
