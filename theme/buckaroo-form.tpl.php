5b17c0bec7a996fb638be4300b95ba4e952120af
Gabor Szanto <hello@szantogabor.com>
2011-11-30 16:51:46 +0100

    Add basic credit card form

--- theme/buckaroo-form.tpl.php
+++ theme/buckaroo-form.tpl.php
@@ -10,6 +10,10 @@
   <?php if ($method_title) : ?>
     <h3 class="title"><?php print $method_title;?></h3>
   <?php endif; ?>
+
+  <?php if ($method_title) : ?>
+    <h3 class="title"><?php print $method_title;?></h3>
+  <?php endif; ?>
   <?php foreach ($method_forms as $method) :?>
     <?php print render($method); ?>
   <?php endforeach;?>
