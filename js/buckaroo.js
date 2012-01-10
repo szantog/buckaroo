50d1a4459fe478753d18b413c976c4a566d621e2
Gabor Szanto <hello@szantogabor.com>
2012-01-10 08:25:38 +0100

    Fix validation, add function to prepare form before submitting.

--- js/buckaroo.js
+++ js/buckaroo.js
@@ -10,8 +10,13 @@ var Buckaroo = {};
   var BuckarooContext = '#buckaroo-payment-form'
   var WrongKey = false;
 
+
   Drupal.behaviors.buckaroo = {
     attach: function (context, settings) {
+
+      var BPE_Amount = $('input[name=BPE_Amount]');
+
+      BPE_Amount.val($('#edit-amount :radio:first', BuckarooContext).val() * 100);
 
       // Remove checked, if uniqueamont field get focus.
       $('#edit-uniqueamount', BuckarooContext).focus (function () {
@@ -19,17 +24,28 @@ var Buckaroo = {};
       })
 
       // If no unique amount value, make the first radio checked.
+      // else set the BPE_value
       $('#edit-uniqueamount', BuckarooContext).blur (function () {
         if ($(this).val == '') {
           $('#edit-amount :radio:first', BuckarooContext).attr('checked', true);
         }
+        else {
+          BPE_Amount.val($(this).val() * 100);
+        }
+      })
+
+      // If a radio is clicked, remove the uniqeamount value, and set BPE_value
+      $('#edit-amount :radio', BuckarooContext).click (function () {
+        $('#edit-uniqueamount', BuckarooContext).val('');
+        ammount = $(this).val();
+        BPE_Amount.val($(this).val() * 100);
       })
 
       // Disable the non-numeric characters.
       $('#edit-uniqueamount', BuckarooContext).keydown(function(event) {
         var msg = $('<div class="clear error">' + Drupal.t('Only numbers are allowed.') + '</div>');
-        // Allow only backspace and delete
-        if ( event.keyCode == 46 || event.keyCode == 8 ) {
+        // Allow only backspace and delete and tab
+        if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9) {
           WrongKey = false;
         }
         else {
@@ -49,18 +65,26 @@ var Buckaroo = {};
 
       // Basic form validation, and pass values to hidden elements. If something
       // wrong, the normal validate process continue via drupal.
-      $("#buckaroo-payment-form .form-submit", BuckarooContext).click(function(e) {
+      $("#buckaroo-payment-form .form-submit", context).click(function(e) {
         var name = $(this).attr('name');
         eval('var FormAction = settings.Buckaroo.' + name + '.action;');
 
         if (Buckaroo.valid()) {
-          BuckarooForm.attr("action", FormAction);
-          e.preventDefault();
+          //BuckarooForm.Prepare();
+          $('#buckaroo-payment-form').attr("action", FormAction);
+          //e.preventDefault();
         }
         else {
           e.preventDefault();
         }
       })
+
+      BuckarooForm.Prepare = function() {
+        $('input[type!="hidden"]', BuckarooContext).each(function(){
+          var html = $('<input type="hidden" value="' + $(this).val() + '" name="' + $(this).attr('name') + '">');
+          $('#buckaroo-payment-form div:first').prepend(html);
+        });
+      }
     }
   };
 
